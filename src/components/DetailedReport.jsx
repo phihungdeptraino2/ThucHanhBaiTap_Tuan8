597724b24ee3cc2460f2e2eb44de6d14fb49082f
phihungdeptraino2 <hungdz1372004@gmail.com>
2025-04-16 19:02:09 +0700

    Cau3. Datatable

--- src/components/DetailedReport.jsx
+++ src/components/DetailedReport.jsx
@@ -5,7 +5,75 @@ import { useEffect, useState } from "react";
 import "../styles/DetailedReportStyle.css";
 
 const DetailedReport = () => {
-  return <div></div>;
+  const columns = [
+    {
+      name: "",
+      cell: () => <input type="checkbox" />,
+      width: "60px",
+    },
+    {
+      name: "CUSTOMER NAME",
+      selector: (row) => row.name,
+      cell: (row) => (
+        <div className="customer-info">
+          <img src={avatar1} alt={avatar1} className="avatar" />
+          <span className="customer-name">{row.name}</span>
+        </div>
+      ),
+      sortable: true,
+    },
+    {
+      name: "COMPANY",
+      selector: (row) => row.company,
+      sortable: true,
+    },
+    {
+      name: "ORDER VALUE",
+      selector: (row) => row.value,
+      sortable: true,
+    },
+    {
+      name: "ORDER DATE",
+      selector: (row) => row.date,
+      sortable: true,
+    },
+    {
+      name: "STATUS",
+      selector: (row) => row.status,
+      cell: (row) => (
+        <span
+          className={`status-badge ${row.status
+            .toLowerCase()
+            .replace(" ", "-")}`}
+        >
+          {row.status}
+        </span>
+      ),
+      sortable: true,
+    },
+    {
+      name: "",
+      cell: (row) => (
+        <button onClick={() => handleEditClick(row.id)}>
+          <Pencil />
+        </button>
+      ),
+      width: "70px",
+    },
+  ];
+  return (
+    <div>
+      <DataTable
+        columns={columns}
+        data={[]}
+        pagination
+        highlightOnHover
+        noDataComponent={
+          <div style={{ padding: "20px" }}>DataTable Không có dữ liệu</div>
+        }
+      />
+    </div>
+  );
 };
 
 export default DetailedReport;
